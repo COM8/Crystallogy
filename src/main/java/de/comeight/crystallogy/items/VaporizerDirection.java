5f3e55184f90b2ab5b44351bb7b1f3135656f931
Fabian Sauter <sauter.fabian@online.de>
2016-04-09 17:41:37 +0200

    Added a max durability

--- src/main/java/de/comeight/crystallogy/items/VaporizerDirection.java
+++ src/main/java/de/comeight/crystallogy/items/VaporizerDirection.java
@@ -1,6 +1,11 @@
 package de.comeight.crystallogy.items;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
@@ -11,6 +16,7 @@ public class VaporizerDirection extends Vaporizer {
 	//-----------------------------------------------Constructor:-------------------------------------------
 	public VaporizerDirection() {
 		super(ID);
+		setMaxDamage(200);
 	}
 	
 	//-----------------------------------------------Set-, Get-Methoden:------------------------------------
@@ -19,8 +25,18 @@ public class VaporizerDirection extends Vaporizer {
 	//-----------------------------------------------Sonstige Methoden:-------------------------------------
 	@Override
 	public void spawnParticles(Vec3d coords, ItemStack stack, World worldIn) {
-		
+		System.out.println(stack.getItemDamage());
+		if(stack.getItemDamage() < 200){
+			stack.setItemDamage(stack.getItemDamage() + 1);
+		}
 	}
 	
-	
+	@Override
+	public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		if(stack.getItemDamage() < 200){
+			stack.setItemDamage(stack.getItemDamage() + 1);
+			return super.onItemUse(stack, playerIn, worldIn, pos, hand, facing, hitX, hitY, hitZ);
+		}
+		return EnumActionResult.FAIL;
+	}
 }
