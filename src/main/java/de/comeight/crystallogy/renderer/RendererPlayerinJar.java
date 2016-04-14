eb0f215e67113a23ad4463a54e7fd44dd6b6aafa
Fabian Sauter <sauter.fabian@online.de>
2016-04-14 18:45:29 +0200

    Code cleanup

--- src/main/java/de/comeight/crystallogy/renderer/RendererPlayerinJar.java
+++ src/main/java/de/comeight/crystallogy/renderer/RendererPlayerinJar.java
@@ -4,6 +4,8 @@ import de.comeight.crystallogy.entity.PlayerClientDummy;
 import de.comeight.crystallogy.tileEntitys.TileEntityPlayerJar;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.VertexBuffer;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.world.World;
 
@@ -21,10 +23,8 @@ public class RendererPlayerinJar extends TileEntitySpecialRenderer<TileEntityPla
 	@Override
 	public void renderTileEntityAt(TileEntityPlayerJar tE, double x, double y, double z, float partialTicks, int destroyStage) {
 		float partialTick = (float) (720.0 * (System.currentTimeMillis() / 2 & 0x3FFFL) / 0x3FFFL);
-		GlStateManager.pushMatrix();
 		renderPlayer(tE.getPlayer(), x, y, z, partialTick);
 		renderIngredients(tE.getWorld(), x, y, z, partialTick);
-		GlStateManager.popMatrix();
 	}
 	
 	private void renderPlayer(PlayerClientDummy player, double posX, double posY, double posZ, float partialTicks){
@@ -39,8 +39,10 @@ public class RendererPlayerinJar extends TileEntitySpecialRenderer<TileEntityPla
 	}
 	
 	private void renderIngredients(World worldIn, double x, double y, double z, float partialTicks){
+		Tessellator tessellator = Tessellator.getInstance();
+		VertexBuffer vB = tessellator.getBuffer();
 		GlStateManager.pushMatrix();
-        
+		
 		GlStateManager.popMatrix();
 	}
 	
