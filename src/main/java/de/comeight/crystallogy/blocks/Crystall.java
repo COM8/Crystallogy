0c7a383ee439ad86b95e5ba0cce1b147a1ec43aa
Fabian Sauter <sauter.fabian@online.de>
2016-05-05 11:09:35 +0200

    Made crystals rotatable

--- src/main/java/de/comeight/crystallogy/blocks/Crystall.java
+++ src/main/java/de/comeight/crystallogy/blocks/Crystall.java
@@ -2,6 +2,8 @@ package de.comeight.crystallogy.blocks;
 
 import java.util.Random;
 
+import com.google.common.base.Predicate;
+
 import de.comeight.crystallogy.CommonProxy;
 import de.comeight.crystallogy.handler.BlockHandler;
 import de.comeight.crystallogy.network.NetworkPacketParticle;
@@ -12,6 +14,9 @@ import de.comeight.crystallogy.util.RGBColor;
 import de.comeight.crystallogy.util.Utilities;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
@@ -19,6 +24,9 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
@@ -30,9 +38,22 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public abstract class Crystall extends BaseBlockCutout{
 	
 	//-----------------------------------------------Variabeln:---------------------------------------------
-	//public final static String ID = "crystall";
 	protected RGBColor color;
 	protected int chance;
+	
+	public static final PropertyDirection FACING = PropertyDirection.create("facing", new Predicate<EnumFacing>()
+    {
+        public boolean apply(EnumFacing p_apply_1_)
+        {
+            return true;
+        }
+    });
+	protected static final AxisAlignedBB CRYSTAL_NORTH_AABB = new AxisAlignedBB(0.3, 0.25, 0.4, 0.7, 0.9, 1.0);
+    protected static final AxisAlignedBB CRYSTAL_SOUTH_AABB = new AxisAlignedBB(0.3, 0.25, 0.0, 0.7, 0.9, 0.6);
+    protected static final AxisAlignedBB CRYSTAL_WEST_AABB = new AxisAlignedBB(0.4, 0.1, 0.3, 1.0, 0.75, 0.7);
+    protected static final AxisAlignedBB CRYSTAL_EAST_AABB = new AxisAlignedBB(0.0, 0.25, 0.3, 0.6, 0.9, 0.7);
+    protected static final AxisAlignedBB CRYSTAL_UP_AABB = new AxisAlignedBB(0.3, 0.0, 0.25, 0.7, 0.6, 0.9);
+    protected static final AxisAlignedBB CRYSTAL_DOWN_AABB = new AxisAlignedBB(0.3, 0.4, 0.25, 0.7, 1.0, 0.9);
 	//-----------------------------------------------Constructor:-------------------------------------------
 	public Crystall(String ID) {
 		super(Material.glass, ID);
@@ -44,13 +65,67 @@ public abstract class Crystall extends BaseBlockCutout{
 		this.setLightLevel(0.3F);
 		this.setHardness(5.0F);
 		this.setStepSound(SoundType.GLASS);
-		
+		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
 	}
 	
 	//-----------------------------------------------Set-, Get-Methoden:------------------------------------
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		EnumFacing enumfacing = EnumFacing.getFront(meta);
+
+		if (enumfacing.getAxis() == EnumFacing.Axis.Y) {
+			enumfacing = EnumFacing.NORTH;
+		}
+
+		return this.getDefaultState().withProperty(FACING, enumfacing);
+	}
+
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return ((EnumFacing) state.getValue(FACING)).getIndex();
+	}
 	
+	private void setDefaultStatus(World worldIn, BlockPos pos, IBlockState state)
+    {
+        if (!worldIn.isRemote)
+        {
+            IBlockState iblockstate = worldIn.getBlockState(pos.north());
+            IBlockState iblockstate1 = worldIn.getBlockState(pos.south());
+            IBlockState iblockstate2 = worldIn.getBlockState(pos.west());
+            IBlockState iblockstate3 = worldIn.getBlockState(pos.east());
+            IBlockState iblockstate4 = worldIn.getBlockState(pos.up());
+            IBlockState iblockstate5 = worldIn.getBlockState(pos.down());
+            EnumFacing enumfacing = (EnumFacing)state.getValue(FACING);
+
+            if (enumfacing == EnumFacing.NORTH && iblockstate.isFullBlock() && !iblockstate1.isFullBlock())
+            {
+                enumfacing = EnumFacing.SOUTH;
+            }
+            else if (enumfacing == EnumFacing.SOUTH && iblockstate1.isFullBlock() && !iblockstate.isFullBlock())
+            {
+                enumfacing = EnumFacing.NORTH;
+            }
+            else if (enumfacing == EnumFacing.WEST && iblockstate2.isFullBlock() && !iblockstate3.isFullBlock())
+            {
+                enumfacing = EnumFacing.EAST;
+            }
+            else if (enumfacing == EnumFacing.EAST && iblockstate3.isFullBlock() && !iblockstate2.isFullBlock())
+            {
+                enumfacing = EnumFacing.WEST;
+            }
+            else if (enumfacing == EnumFacing.UP && iblockstate4.isFullBlock() && !iblockstate4.isFullBlock())
+            {
+                enumfacing = EnumFacing.DOWN;
+            }
+            else if (enumfacing == EnumFacing.DOWN && iblockstate5.isFullBlock() && !iblockstate5.isFullBlock())
+            {
+                enumfacing = EnumFacing.UP;
+            }
+
+            worldIn.setBlockState(pos, state.withProperty(FACING, enumfacing), 2);
+        }
+    }
 	
-	//-----------------------------------------------Sonstige Methoden:-------------------------------------
 	@Override
 	public String getHarvestTool(IBlockState state) {
 		return "pickaxe";
@@ -58,9 +133,25 @@ public abstract class Crystall extends BaseBlockCutout{
 	
 	@Override
 	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-		return new AxisAlignedBB(0.3, 0.0, 0.25, 0.7, 0.6, 0.9);
+		switch ((EnumFacing)state.getValue(FACING)){
+			case NORTH:
+				return CRYSTAL_NORTH_AABB;
+			case SOUTH:
+				return CRYSTAL_SOUTH_AABB;
+			case EAST:
+				return CRYSTAL_EAST_AABB;
+			case WEST:
+				return CRYSTAL_WEST_AABB;
+			case UP:
+				return CRYSTAL_UP_AABB;
+			case DOWN:
+				return CRYSTAL_DOWN_AABB;
+			default:
+				return CRYSTAL_DOWN_AABB;
+		}
 	}
 	
+	//-----------------------------------------------Sonstige Methoden:-------------------------------------
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void randomDisplayTick(IBlockState state, World worldIn, BlockPos pos, Random rand) {
@@ -163,5 +254,46 @@ public abstract class Crystall extends BaseBlockCutout{
     		entityIn.attackEntityFrom(DamageSource.generic, 0.5F);
     	}
 
+    }
+    
+    @Override
+	public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state)
+    {
+        setDefaultStatus(worldIn, pos, state);
+    }
+
+    @Override
+    public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+    {
+    	IBlockState iblockstate = worldIn.getBlockState(pos.offset(facing.getOpposite()));
+    	
+    	if(iblockstate.getBlock() instanceof Crystall){
+    		EnumFacing enumfacing = (EnumFacing)iblockstate.getValue(FACING);
+
+            if (enumfacing == facing)
+            {
+                return this.getDefaultState().withProperty(FACING, facing.getOpposite());
+            }
+    	}
+        
+        return this.getDefaultState().withProperty(FACING, facing);
+    }
+    
+    @Override
+    public IBlockState withRotation(IBlockState state, Rotation rot)
+    {
+        return state.withProperty(FACING, rot.rotate((EnumFacing)state.getValue(FACING)));
+    }
+
+    @Override
+    public IBlockState withMirror(IBlockState state, Mirror mirrorIn)
+    {
+        return state.withRotation(mirrorIn.toRotation((EnumFacing)state.getValue(FACING)));
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, new IProperty[] {FACING});
     }
 }
