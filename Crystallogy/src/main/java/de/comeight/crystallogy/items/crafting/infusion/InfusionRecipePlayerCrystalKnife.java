b5dd522fe767d84e52288bb3c2557d23122e3f45
Fabian Sauter <sauter.fabian@online.de>
2016-05-17 10:42:28 +0200

    Added a entityCrystalKnife recipe

--- Crystallogy/src/main/java/de/comeight/crystallogy/items/crafting/infusion/InfusionRecipePlayerCrystalKnife.java
+++ Crystallogy/src/main/java/de/comeight/crystallogy/items/crafting/infusion/InfusionRecipePlayerCrystalKnife.java
@@ -1,17 +1,16 @@
 package de.comeight.crystallogy.items.crafting.infusion;
 
 import de.comeight.crystallogy.handler.ItemHandler;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
-public class InfusionRecipeCrystalKnife extends InfusionRecipeSimple {
+public class InfusionRecipePlayerCrystalKnife extends InfusionRecipeSimple {
 	//-----------------------------------------------Variabeln:---------------------------------------------
 
 	
 	//-----------------------------------------------Constructor:-------------------------------------------
-	public InfusionRecipeCrystalKnife() {
-		super("crystalKnife", 500, new ItemStack(ItemHandler.crystalKnifeBlade),
+	public InfusionRecipePlayerCrystalKnife() {
+		super("playerCrystalKnife", 500, new ItemStack(ItemHandler.crystalKnifeBlade),
 				new ItemStack[]{ 	new ItemStack(ItemHandler.toolRod, 1),
 									new ItemStack(Items.ender_eye, 1),
 									new ItemStack(Items.blaze_powder, 1),
@@ -25,7 +24,7 @@ public class InfusionRecipeCrystalKnife extends InfusionRecipeSimple {
 	//-----------------------------------------------Sonstige Methoden:-------------------------------------
 	@Override
 	public InfusionRecipe getRecipe() {
-		return new InfusionRecipeCrystalKnife();
+		return new InfusionRecipePlayerCrystalKnife();
 	}
 	
 }
