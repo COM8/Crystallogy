8e870540a488b499aedec419e9dbeb6838919ec1
Fabian Sauter <sauter.fabian@online.de>
2016-05-19 12:18:31 +0200

    Tweaked the tool materials a bit

--- Crystallogy/src/main/java/de/comeight/crystallogy/blocks/materials/CustomToolMaterials.java
+++ Crystallogy/src/main/java/de/comeight/crystallogy/blocks/materials/CustomToolMaterials.java
@@ -6,10 +6,10 @@ import net.minecraftforge.common.util.EnumHelper;
 
 public class CustomToolMaterials {
 	//-----------------------------------------------Variabeln:---------------------------------------------
-	public static ToolMaterial CRYSTALL_RED = EnumHelper.addToolMaterial("crystal_red", 3, 1300, 3.0F, 7.5F, 12);
-	public static ToolMaterial CRYSTALL_BLUE = EnumHelper.addToolMaterial("crystal_blue", 2, 350, 2.0F, 5.5F, 8);
-	public static ToolMaterial CRYSTALL_GREEN = EnumHelper.addToolMaterial("crystal_green", 2, 250, 2.0F, 5.5F, 8);
-	public static ToolMaterial CRYSTALL_YELLOW = EnumHelper.addToolMaterial("crystal_yellow", 2, 350, 2.0F, 5.5F, 8);
+	public static ToolMaterial CRYSTALL_RED = EnumHelper.addToolMaterial("crystal_red", 3, 1300, 5.0F, 7.5F, 12);
+	public static ToolMaterial CRYSTALL_BLUE = EnumHelper.addToolMaterial("crystal_blue", 2, 350, 3.0F, 5.5F, 8);
+	public static ToolMaterial CRYSTALL_GREEN = EnumHelper.addToolMaterial("crystal_green", 2, 250, 2.5F, 5.5F, 8);
+	public static ToolMaterial CRYSTALL_YELLOW = EnumHelper.addToolMaterial("crystal_yellow", 3, 350, 4.2F, 5.5F, 8);
 	
 	public static final ToolMaterial CRYSTALLKNIF = EnumHelper.addToolMaterial("crystalKnife", 2, 10, 6.0F, 1.0F, 17);
 	
