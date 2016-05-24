78e7d44d9ebd8ec2a1a48375d5a563ddbdb36eb4
Fabian Sauter <sauter.fabian@online.de>
2016-05-24 10:17:49 +0200

    Added an entity grabber

--- Crystallogy/src/main/java/de/comeight/crystallogy/items/tools/EntityGrabber.java
+++ Crystallogy/src/main/java/de/comeight/crystallogy/items/tools/EntityGrabber.java
@@ -1,6 +1,9 @@
-package de.comeight.crystallogy.items;
+package de.comeight.crystallogy.items.tools;
+
+import java.util.List;
 
 import de.comeight.crystallogy.handler.BlockHandler;
+import de.comeight.crystallogy.items.BaseItem;
 import de.comeight.crystallogy.util.EntityUtils;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
@@ -9,6 +12,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.text.TextFormatting;
 
 public class EntityGrabber extends BaseItem {
 	//-----------------------------------------------Variabeln:---------------------------------------------
@@ -23,6 +27,13 @@ public class EntityGrabber extends BaseItem {
 
 	
 	//-----------------------------------------------Sonstige Methoden:-------------------------------------
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced) {
+		tooltip.add("Right klick to grab an entity.");
+		tooltip.add("Requires an empty " + TextFormatting.UNDERLINE + TextFormatting.DARK_PURPLE + "Crystal Of Holding" + TextFormatting.RESET + "!");
+		super.addInformation(stack, playerIn, tooltip, advanced);
+	}
+	
 	@Override
 	public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer playerIn, EntityLivingBase target, EnumHand hand) {
 		if(playerIn.worldObj.isRemote){
