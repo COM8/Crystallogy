4abba9b925f01e2b39e0640fdc65aed25a50a169
Fabian Sauter <sauter.fabian@online.de>
2016-05-25 17:40:58 +0200

    Reimplemented custom armor material (Forge Fix #2904)

--- Crystallogy/src/main/java/de/comeight/crystallogy/blocks/materials/CustomArmorMaterials.java
+++ Crystallogy/src/main/java/de/comeight/crystallogy/blocks/materials/CustomArmorMaterials.java
@@ -7,18 +7,12 @@ import net.minecraftforge.common.util.EnumHelper;
 
 public class CustomArmorMaterials {
 	//-----------------------------------------------Variabeln:---------------------------------------------
-	/*public static ArmorMaterial CRYSTALL_RED = EnumHelper.addEnum(ArmorMaterial.class, CrystallogyBase.MODID + ":red", 20, new int[]{4,6,6,4}, 20, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.0F);
-	public static ArmorMaterial CRYSTALL_BLUE = EnumHelper.addEnum(ArmorMaterial.class, CrystallogyBase.MODID + ":blue", 7, new int[]{2,3,3,2}, 7, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.0F);
-	public static ArmorMaterial CRYSTALL_GREEN = EnumHelper.addEnum(ArmorMaterial.class, CrystallogyBase.MODID + ":green", 10, new int[]{2,4,4,2}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.0F);
-	public static ArmorMaterial CRYSTALL_YELLOW = EnumHelper.addEnum(ArmorMaterial.class, CrystallogyBase.MODID + ":yellow", 15, new int[]{3,5,5,3}, 15, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.0F);
-	public static ArmorMaterial CRYSTALL_COMBINED = EnumHelper.addEnum(ArmorMaterial.class, CrystallogyBase.MODID + ":combined", 15, new int[]{2,7,7,4}, 15, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.0F);*/
+	public static ArmorMaterial CRYSTALL_RED = EnumHelper.addArmorMaterial("crystal_red", CrystallogyBase.MODID + ":red", 20, new int[]{4,6,6,4}, 20, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.0F);
+	public static ArmorMaterial CRYSTALL_BLUE = EnumHelper.addArmorMaterial("crystal_blue", CrystallogyBase.MODID + ":blue", 7, new int[]{2,3,3,2}, 7, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.0F);
+	public static ArmorMaterial CRYSTALL_GREEN = EnumHelper.addArmorMaterial("crystal_green", CrystallogyBase.MODID + ":green", 10, new int[]{2,4,4,2}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.0F);
+	public static ArmorMaterial CRYSTALL_YELLOW = EnumHelper.addArmorMaterial("crystal_yellow", CrystallogyBase.MODID + ":yellow", 15, new int[]{3,5,5,3}, 15, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.0F);
+	public static ArmorMaterial CRYSTALL_COMBINED = EnumHelper.addArmorMaterial("crystal_combined", CrystallogyBase.MODID + ":combined", 15, new int[]{2,7,7,4}, 15, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.0F);
 	
-	//TODO FIX Armor Material
-	public static ArmorMaterial CRYSTALL_RED = ArmorMaterial.DIAMOND;
-	public static ArmorMaterial CRYSTALL_BLUE = ArmorMaterial.DIAMOND;
-	public static ArmorMaterial CRYSTALL_GREEN = ArmorMaterial.DIAMOND;
-	public static ArmorMaterial CRYSTALL_YELLOW = ArmorMaterial.DIAMOND;
-	public static ArmorMaterial CRYSTALL_COMBINED = ArmorMaterial.DIAMOND;
 	//-----------------------------------------------Constructor:-------------------------------------------
 
 	
