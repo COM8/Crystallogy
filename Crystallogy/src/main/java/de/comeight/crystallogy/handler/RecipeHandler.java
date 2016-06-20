50d6d502d0e987b91130bc788baf0c5650f36f13
Fabian Sauter <sauter.fabian@online.de>
2016-06-20 17:12:28 +0200

    Outsourced and cleaned up crafting recipes

---
+++ Crystallogy/src/main/java/de/comeight/crystallogy/handler/RecipeHandler.java
@@ -0,0 +1,401 @@
+package de.comeight.crystallogy.handler;
+
+import de.comeight.crystallogy.blocks.EnumCrystalColor;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeArmorCatalyst;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeArmorCombinedBoots;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeArmorCombinedChestplate;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeArmorCombinedHelmet;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeArmorCombinedLeggins;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeArmorPlate;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeBadLuckDust;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeBlindDust;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeCombinedArmorCompound;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeCrystalKnifeBlade;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeCrystalOfHolding;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeCrystallLight;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeDamDust;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeDrowDust;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeEnderDust;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeEntityCrystalKnife;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeEntityJar;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeFireCrystall;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeFireDust;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeGlowDust;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeHammer;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeHungDust;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeLevDust;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeMachineBlock;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipePickaxe;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipePlayerCrystalKnife;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipePlayerJar;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipePoisDust;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipePureDust;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeRefulelVaporizer;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeSword;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeToolRod;
+import de.comeight.crystallogy.items.crafting.infusion.InfusionRecipeVaporizer;
+import de.comeight.crystallogy.util.Utilities;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+
+public class RecipeHandler {
+	//-----------------------------------------------Variabeln:---------------------------------------------
+	//Infusion Recipes:
+		private static InfusionRecipeVaporizer infusionRecipeVaporizer; 
+		private static InfusionRecipeCrystallLight infusionRecipeCrystallLight;
+		private static InfusionRecipeFireCrystall infusionRecipeFireCrystall;
+		private static InfusionRecipeRefulelVaporizer infusionRecipeRefulelVaporizer;
+		private static InfusionRecipeToolRod infusionRecipeToolRod;
+		private static InfusionRecipePureDust infusionRecipePureDust;
+		private static InfusionRecipeHammer infusionRecipeHammer;
+		private static InfusionRecipePickaxe infusionRecipePickaxe;
+		private static InfusionRecipeSword infusionRecipeSword;
+		private static InfusionRecipeHungDust infusionRecipeHungDust;
+		private static InfusionRecipeDamDust infusionRecipeDamDust;
+		private static InfusionRecipeFireDust infusionRecipeFireDust;
+		private static InfusionRecipeDrowDust infusionRecipeDrowDust;
+		private static InfusionRecipePoisDust infusionRecipePoisDust;
+		private static InfusionRecipeBadLuckDust infusionRecipeBadLuckDust;
+		private static InfusionRecipeBlindDust infusionRecipeBlindDust;
+		private static InfusionRecipeEnderDust infusionRecipeEnderDust;
+		private static InfusionRecipeGlowDust infusionRecipeGlowDust;
+		private static InfusionRecipeLevDust infusionRecipeLevDust;
+		private static InfusionRecipePlayerCrystalKnife infusionRecipePlayerCrystalKnife;
+		private static InfusionRecipeEntityCrystalKnife infusionRecipeEntityCrystalKnife;
+		private static InfusionRecipeCrystalKnifeBlade infusionRecipeCrystalKnifeBlade;
+		private static InfusionRecipePlayerJar infusionRecipePlayerJar;
+		private static InfusionRecipeEntityJar infusionRecipeEntityJar;
+		private static InfusionRecipeArmorPlate infusionRecipeArmorPlate;
+		private static InfusionRecipeArmorCombinedHelmet infusionRecipeArmorCombinedHelmet;
+		private static InfusionRecipeArmorCombinedChestplate infusionRecipeArmorCombinedChestplate;
+		private static InfusionRecipeArmorCombinedLeggins infusionRecipeArmorCombinedLeggins;
+		private static InfusionRecipeArmorCombinedBoots infusionRecipeArmorCombinedBoots;
+		private static InfusionRecipeCrystalOfHolding infusionRecipeCrystalOfHolding;
+		private static InfusionRecipeMachineBlock infusionRecipeMachineBlock;
+		private static InfusionRecipeArmorCatalyst infusionRecipeArmorCatalyst;
+		private static InfusionRecipeCombinedArmorCompound infusionRecipeCombinedArmorCompound;
+
+	//-----------------------------------------------Constructor:-------------------------------------------
+
+
+	//-----------------------------------------------Set-, Get-Methoden:------------------------------------
+
+
+	//-----------------------------------------------Sonstige Methoden:-------------------------------------
+	private void registerInfusionRecipes(){
+		infusionRecipeVaporizer = new InfusionRecipeVaporizer(); 
+		infusionRecipeCrystallLight = new InfusionRecipeCrystallLight();
+		infusionRecipeFireCrystall = new InfusionRecipeFireCrystall();
+		infusionRecipeRefulelVaporizer = new InfusionRecipeRefulelVaporizer();
+		infusionRecipeToolRod = new InfusionRecipeToolRod();
+		infusionRecipePureDust = new InfusionRecipePureDust();
+		infusionRecipeHammer = new InfusionRecipeHammer();
+		infusionRecipeSword = new InfusionRecipeSword();
+		infusionRecipeHungDust = new InfusionRecipeHungDust();
+		infusionRecipeDamDust = new InfusionRecipeDamDust();
+		infusionRecipeFireDust = new InfusionRecipeFireDust();
+		infusionRecipeDrowDust = new InfusionRecipeDrowDust();
+		infusionRecipePoisDust = new InfusionRecipePoisDust();
+		infusionRecipeBadLuckDust = new InfusionRecipeBadLuckDust();
+		infusionRecipeBlindDust = new InfusionRecipeBlindDust();
+		infusionRecipeEnderDust = new InfusionRecipeEnderDust();
+		infusionRecipeGlowDust = new InfusionRecipeGlowDust();
+		infusionRecipeLevDust = new InfusionRecipeLevDust();
+		infusionRecipePlayerCrystalKnife = new InfusionRecipePlayerCrystalKnife();
+		infusionRecipeEntityCrystalKnife = new InfusionRecipeEntityCrystalKnife();
+		infusionRecipeCrystalKnifeBlade = new InfusionRecipeCrystalKnifeBlade();
+		infusionRecipePlayerJar = new InfusionRecipePlayerJar();
+		infusionRecipeArmorPlate = new InfusionRecipeArmorPlate();
+		infusionRecipeArmorCombinedHelmet = new InfusionRecipeArmorCombinedHelmet();
+		infusionRecipeArmorCombinedChestplate = new InfusionRecipeArmorCombinedChestplate();
+		infusionRecipeArmorCombinedLeggins = new InfusionRecipeArmorCombinedLeggins();
+		infusionRecipeArmorCombinedBoots = new InfusionRecipeArmorCombinedBoots();
+		infusionRecipeEntityJar = new InfusionRecipeEntityJar();
+		infusionRecipePickaxe = new InfusionRecipePickaxe();
+		infusionRecipeCrystalOfHolding = new InfusionRecipeCrystalOfHolding();
+		infusionRecipeMachineBlock = new InfusionRecipeMachineBlock();
+		infusionRecipeArmorCatalyst = new InfusionRecipeArmorCatalyst();
+		infusionRecipeCombinedArmorCompound = new InfusionRecipeCombinedArmorCompound();
+		
+		InfusionRecipeHandler.addRecipe(infusionRecipeCombinedArmorCompound);
+		InfusionRecipeHandler.addRecipe(infusionRecipeArmorCatalyst);
+		InfusionRecipeHandler.addRecipe(infusionRecipeMachineBlock);
+		InfusionRecipeHandler.addRecipe(infusionRecipeCrystalOfHolding);
+		InfusionRecipeHandler.addRecipe(infusionRecipeVaporizer);
+		InfusionRecipeHandler.addRecipe(infusionRecipeCrystallLight);
+		InfusionRecipeHandler.addRecipe(infusionRecipeFireCrystall);
+		InfusionRecipeHandler.addRecipe(infusionRecipeRefulelVaporizer);
+		InfusionRecipeHandler.addRecipe(infusionRecipePureDust);
+		InfusionRecipeHandler.addRecipe(infusionRecipeSword);
+		InfusionRecipeHandler.addRecipe(infusionRecipeHammer);
+		InfusionRecipeHandler.addRecipe(infusionRecipeToolRod);
+		InfusionRecipeHandler.addRecipe(infusionRecipeDamDust);
+		InfusionRecipeHandler.addRecipe(infusionRecipeDrowDust);
+		InfusionRecipeHandler.addRecipe(infusionRecipeFireDust);
+		InfusionRecipeHandler.addRecipe(infusionRecipePoisDust);
+		InfusionRecipeHandler.addRecipe(infusionRecipeHungDust);
+		InfusionRecipeHandler.addRecipe(infusionRecipeBadLuckDust);
+		InfusionRecipeHandler.addRecipe(infusionRecipeBlindDust);
+		InfusionRecipeHandler.addRecipe(infusionRecipeEnderDust);
+		InfusionRecipeHandler.addRecipe(infusionRecipeGlowDust);
+		InfusionRecipeHandler.addRecipe(infusionRecipeLevDust);
+		InfusionRecipeHandler.addRecipe(infusionRecipePlayerCrystalKnife);
+		InfusionRecipeHandler.addRecipe(infusionRecipeEntityCrystalKnife);
+		InfusionRecipeHandler.addRecipe(infusionRecipeCrystalKnifeBlade);
+		InfusionRecipeHandler.addRecipe(infusionRecipePlayerJar);
+		InfusionRecipeHandler.addRecipe(infusionRecipeEntityJar);
+		InfusionRecipeHandler.addRecipe(infusionRecipeArmorPlate);
+		InfusionRecipeHandler.addRecipe(infusionRecipeArmorCombinedHelmet);
+		InfusionRecipeHandler.addRecipe(infusionRecipeArmorCombinedChestplate);
+		InfusionRecipeHandler.addRecipe(infusionRecipeArmorCombinedLeggins);
+		InfusionRecipeHandler.addRecipe(infusionRecipeArmorCombinedBoots);
+		InfusionRecipeHandler.addRecipe(infusionRecipePickaxe);
+	}
+
+	private void registerCraftingRecipes(){
+		GameRegistry.addRecipe(new ShapedOreRecipe(ItemHandler.vaporizer.getItemStack(), new Object[]{
+				"III",
+				"PI_",
+				"_G_",
+				
+				'I', Items.IRON_INGOT,
+				'P', "plankWood",
+				'G', "blockGlassLime",
+		}));
+		
+		GameRegistry.addRecipe(new ShapedOreRecipe(BlockHandler.infuserBlock, new Object[]{
+				"CIC",
+				"_C_",
+				"WWW",
+				
+				'C', "cobblestone",
+				'I', "workbench",
+				'W', Blocks.HEAVY_WEIGHTED_PRESSURE_PLATE,
+		}));
+		
+		addCrystalHammerHeadRecipe(new ItemStack(BlockHandler.crystall_red), EnumCrystalColor.RED.getStack(new ItemStack(ItemHandler.crystallHammerHead)));
+		addCrystalHammerHeadRecipe(new ItemStack(BlockHandler.crystall_blue), EnumCrystalColor.BLUE.getStack(new ItemStack(ItemHandler.crystallHammerHead)));
+		addCrystalHammerHeadRecipe(new ItemStack(BlockHandler.crystall_green), EnumCrystalColor.GREEN.getStack(new ItemStack(ItemHandler.crystallHammerHead)));
+		addCrystalHammerHeadRecipe(new ItemStack(BlockHandler.crystall_yellow), EnumCrystalColor.YELLOW.getStack(new ItemStack(ItemHandler.crystallHammerHead)));
+		
+		addCrystalSwordBladeRecipe(new ItemStack(BlockHandler.crystall_red), EnumCrystalColor.RED.getStack(new ItemStack(ItemHandler.crystallSwordBlade)));
+		addCrystalSwordBladeRecipe(new ItemStack(BlockHandler.crystall_blue), EnumCrystalColor.BLUE.getStack(new ItemStack(ItemHandler.crystallSwordBlade)));
+		addCrystalSwordBladeRecipe(new ItemStack(BlockHandler.crystall_green), EnumCrystalColor.GREEN.getStack(new ItemStack(ItemHandler.crystallSwordBlade)));
+		addCrystalSwordBladeRecipe(new ItemStack(BlockHandler.crystall_yellow), EnumCrystalColor.YELLOW.getStack(new ItemStack(ItemHandler.crystallSwordBlade)));
+
+		GameRegistry.addShapedRecipe(new ItemStack(ItemHandler.armorPlate, 4, 4), new Object[] {
+				"RG",
+				"BY",
+				
+				'R', BlockHandler.crystall_red,
+				'G', BlockHandler.crystall_green,
+				'B', BlockHandler.crystall_blue,
+				'Y', BlockHandler.crystall_yellow
+		});
+		
+		//Armor Red:
+		addArmorRecipe(new ItemStack(ItemHandler.armorPlate, 1, 0), new ItemStack(ItemHandler.armorHelmet_red),
+																	new ItemStack(ItemHandler.armorChestplate_red),
+																	new ItemStack(ItemHandler.armorLeggins_red),
+																	new ItemStack(ItemHandler.armorBoots_red));
+		
+		//Armor Green:
+		addArmorRecipe(new ItemStack(ItemHandler.armorPlate, 1, 2), new ItemStack(ItemHandler.armorHelmet_green),
+																	new ItemStack(ItemHandler.armorChestplate_green),
+																	new ItemStack(ItemHandler.armorLeggins_green),
+																	new ItemStack(ItemHandler.armorBoots_green));
+		
+		//Armor Blue:
+		addArmorRecipe(new ItemStack(ItemHandler.armorPlate, 1, 1), new ItemStack(ItemHandler.armorHelmet_blue),
+																	new ItemStack(ItemHandler.armorChestplate_blue),
+																	new ItemStack(ItemHandler.armorLeggins_blue),
+																	new ItemStack(ItemHandler.armorBoots_blue));
+		
+		//Armor Yellow:
+		addArmorRecipe(new ItemStack(ItemHandler.armorPlate, 1, 3), new ItemStack(ItemHandler.armorHelmet_yellow),
+																	new ItemStack(ItemHandler.armorChestplate_yellow),
+																	new ItemStack(ItemHandler.armorLeggins_yellow),
+																	new ItemStack(ItemHandler.armorBoots_yellow));
+		
+		//Armor Combined:
+		addArmorRecipe(new ItemStack(ItemHandler.combinedArmorCompound), new ItemStack(ItemHandler.armorHelmet_combined),
+				new ItemStack(ItemHandler.armorChestplate_combined),
+				new ItemStack(ItemHandler.armorLeggins_combined),
+				new ItemStack(ItemHandler.armorBoots_combined));
+		
+		GameRegistry.addShapedRecipe(new ItemStack(ItemHandler.energyDust), new Object[] {
+				"RRR",
+				"RCR",
+				"RRR",
+				'C', ItemHandler.crystallDust_red,
+				'R', Items.REDSTONE
+		});
+		
+		GameRegistry.addShapedRecipe(new ItemStack(BlockHandler.armorCombiner), new Object[] {
+				"YDR",
+				"BAG",
+				"IMI",
+				'M', BlockHandler.machineBlock,
+				'R', BlockHandler.crystall_red,
+				'B', BlockHandler.crystall_blue,
+				'G', BlockHandler.crystall_green,
+				'Y', BlockHandler.crystall_yellow,
+				'A', ItemHandler.armorChestplate_combined,
+				'D', Blocks.DIAMOND_BLOCK,
+				'I', ItemHandler.armorCatalys
+				
+		});
+		
+		GameRegistry.addShapedRecipe(new ItemStack(BlockHandler.compressor), new Object[] {
+				"OCO",
+				"ORO",
+				"OMO",
+				'O', Blocks.OBSIDIAN,
+				'R', Items.REDSTONE,
+				'M', BlockHandler.machineBlock,
+				'C', BlockHandler.crystall_blue
+		});
+		
+		GameRegistry.addShapedRecipe(new ItemStack(BlockHandler.crystallCrusher), new Object[] {
+				"BBB",
+				"I_I",
+				"IMI",
+				'B', Blocks.BRICK_BLOCK,
+				'I', Items.IRON_INGOT,
+				'M', Blocks.IRON_BLOCK
+		});
+		
+		GameRegistry.addShapedRecipe(new ItemStack(BlockHandler.charger), new Object[] {
+				"OCO",
+				"ORO",
+				"OMO",
+				'O', Blocks.OBSIDIAN,
+				'R', Items.REDSTONE,
+				'C', new ItemStack(ItemHandler.energyCrystal, 1, 12000),
+				'M', BlockHandler.machineBlock
+		});
+		
+		addCrystalGlasRecipe(new ItemStack(ItemHandler.crystallDust_red), new ItemStack(BlockHandler.crystalGlas, 7, 0));
+		addCrystalGlasRecipe(new ItemStack(ItemHandler.crystallDust_blue), new ItemStack(BlockHandler.crystalGlas, 7, 1));
+		addCrystalGlasRecipe(new ItemStack(ItemHandler.crystallDust_green), new ItemStack(BlockHandler.crystalGlas, 7, 2));
+		addCrystalGlasRecipe(new ItemStack(ItemHandler.crystallDust_yellow), new ItemStack(BlockHandler.crystalGlas, 7, 3));
+		
+		GameRegistry.addShapedRecipe(new ItemStack(ItemHandler.fertilizerPotato, 3), new Object[] {
+				"YPC",
+				"BPB",
+				"CPY",
+				'B', new ItemStack(Items.DYE, 2, 15),
+				'P', Items.POISONOUS_POTATO,
+				'C', ItemHandler.crystallDust_green,
+				'Y', BlockHandler.crystorya
+		});
+		
+		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockHandler.farmersGreen, 1, 1), new Object[] {
+				"IPI",
+				"G_G",
+				"GGG",
+				'I', Items.IRON_INGOT,
+				'P', "plankWood",
+				'G', new ItemStack(BlockHandler.crystalGlas, 1, 2)
+		}));
+		
+		addCrystalPickaxeHeadPartRecipe(new ItemStack(BlockHandler.crystall_red), new ItemStack(ItemHandler.crystalPickaxeHead, 1, 0));
+		addCrystalPickaxeHeadPartRecipe(new ItemStack(BlockHandler.crystall_blue), new ItemStack(ItemHandler.crystalPickaxeHead, 1, 1));
+		addCrystalPickaxeHeadPartRecipe(new ItemStack(BlockHandler.crystall_green), new ItemStack(ItemHandler.crystalPickaxeHead, 1, 2));
+		addCrystalPickaxeHeadPartRecipe(new ItemStack(BlockHandler.crystall_yellow), new ItemStack(ItemHandler.crystalPickaxeHead, 1, 3));
+		
+		GameRegistry.addShapedRecipe(new ItemStack(ItemHandler.entityGrabber), new Object[] {
+				"IB_",
+				"BIY",
+				"__I",
+				'I', Items.IRON_INGOT,
+				'B', new ItemStack(BlockHandler.crystalGlas, 1, 1),
+				'Y', new ItemStack(ItemHandler.armorPlate, 1, 3)
+		});
+	}
+	
+	private void addCrystalHammerHeadRecipe(ItemStack crystal, ItemStack out){
+		GameRegistry.addShapedRecipe(out, new Object[] {
+				"CCC",
+				"CCC",
+				'C', crystal
+		});
+	}
+	
+	private void addCrystalSwordBladeRecipe(ItemStack crystal, ItemStack out){
+		GameRegistry.addShapedRecipe(out, new Object[] {
+				"C",
+				"C",
+				'C', crystal
+		});
+	}
+	
+	private void addCrystalPickaxeHeadPartRecipe(ItemStack crystal, ItemStack out){
+		GameRegistry.addShapedRecipe(out, new Object[] {
+				"CCC",
+				'C', crystal
+		});
+	}
+	
+	private void addCrystalGlasRecipe(ItemStack dust, ItemStack glas){
+		GameRegistry.addRecipe(new ShapedOreRecipe(glas, new Object[]{
+				"GGG",
+				"DGD",
+				"GGG",
+				
+				'G', "blockGlass",
+				'D', dust,
+		}));
+	}
+	
+	private void addArmorRecipe(ItemStack s, ItemStack helmet, ItemStack chestplate, ItemStack leggins, ItemStack boots){
+		GameRegistry.addShapedRecipe(helmet, new Object[] {
+				"SSS",
+				"S_S",
+				
+				'S', s
+		});
+		
+		GameRegistry.addShapedRecipe(chestplate, new Object[] {
+				"S_S",
+				"SSS",
+				"SSS",
+				
+				'S', s
+		});
+		
+		GameRegistry.addShapedRecipe(leggins, new Object[] {
+				"SSS",
+				"S_S",
+				"S_S",
+				
+				'S', s
+		});
+		
+		GameRegistry.addShapedRecipe(boots, new Object[] {
+				"S_S",
+				"S_S",
+				
+				'S', s
+		});
+	}
+	
+	//-----------------------------------------------Pre-Init:----------------------------------------------
+	public void preInit(){
+	}
+
+	//-----------------------------------------------Init:--------------------------------------------------
+	public void init(){
+		registerInfusionRecipes();
+		registerCraftingRecipes();
+		
+		Utilities.addConsoleText("All recipes are registered.");
+	}
+
+	//-----------------------------------------------Post-Init:---------------------------------------------
+	public void postInit(){
+	}
+}
