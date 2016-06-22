d4feac86fabb5929bf3197817c019412d0254691
Fabian Sauter <sauter.fabian@online.de>
2016-06-22 18:05:30 +0200

    Added a recipe for the Combined Armor Compound

--- Crystallogy/src/main/java/de/comeight/crystallogy/handler/CompressorRecipeHandler.java
+++ Crystallogy/src/main/java/de/comeight/crystallogy/handler/CompressorRecipeHandler.java
@@ -12,6 +12,7 @@ public class CompressorRecipeHandler extends BaseRecipeHandler{
 		addRecipe(new RecipeCompressor(new ItemStack[]{new ItemStack(ItemHandler.energyDust, 8)}, new ItemStack[]{new ItemStack(ItemHandler.energyCrystal, 1, ItemHandler.energyCrystal.getMaxDamage())}, 1.0F, 200));
 		
 		addRecipe(new RecipeCompressor(new ItemStack[]{new ItemStack(ItemHandler.hunterArmorMesh, 4)}, new ItemStack[]{new ItemStack(ItemHandler.hunterArmorCompound, 2)}, 1.0F, 1200));
+		addRecipe(new RecipeCompressor(new ItemStack[]{new ItemStack(ItemHandler.chargedCombinedArmorMesh, 4)}, new ItemStack[]{new ItemStack(ItemHandler.combinedArmorCompound, 2)}, 1.0F, 1200));
 	}
 	
 	//-----------------------------------------------Set-, Get-Methoden:------------------------------------
