f221d77d2e85b820570ffb07db8eade78432044d
Fabian Sauter <sauter.fabian@online.de>
2016-06-30 10:57:14 +0200

    Added the @SideOnly(Side.CLIENT) annotation

--- Crystallogy/src/main/java/de/comeight/crystallogy/entity/PlayerClientDummy.java
+++ Crystallogy/src/main/java/de/comeight/crystallogy/entity/PlayerClientDummy.java
@@ -4,7 +4,10 @@ import com.mojang.authlib.GameProfile;
 
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
+@SideOnly(Side.CLIENT)
 public class PlayerClientDummy extends AbstractClientPlayer{
 	//-----------------------------------------------Variabeln:---------------------------------------------
 	
