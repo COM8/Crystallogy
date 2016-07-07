5e2972819eb1c63e2aedce3af107c460f5ee4bac
Fabian Sauter <sauter.fabian@online.de>
2016-07-07 13:01:42 +0200

    Added a custom sound event if crushing

--- Crystallogy/src/main/java/de/comeight/crystallogy/tileEntitys/machines/TileEntityCrystallCrusher.java
+++ Crystallogy/src/main/java/de/comeight/crystallogy/tileEntitys/machines/TileEntityCrystallCrusher.java
@@ -2,6 +2,11 @@ package de.comeight.crystallogy.tileEntitys.machines;
 
 import de.comeight.crystallogy.blocks.machines.CrystallCrusher;
 import de.comeight.crystallogy.handler.CrystalCrusherRecipeHandler;
+import de.comeight.crystallogy.handler.SoundHandler;
+import de.comeight.crystallogy.util.Utilities;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.world.World;
 
 public class TileEntityCrystallCrusher extends BaseTileEntityMachine {
 	//-----------------------------------------------Variabeln:---------------------------------------------
@@ -19,9 +24,19 @@ public class TileEntityCrystallCrusher extends BaseTileEntityMachine {
 		return "de.comeight.crystallogy.tileEntityCrystallCrusher";
     }
 	
+	@Override
+	public int getSoundIntervall() {
+		return 40;
+	}
+	
 	//-----------------------------------------------Sonstige Methoden:-------------------------------------
 	public void setBlockState(){
 		CrystallCrusher.setBlockState(crafting, worldObj, pos);
+	}
+	
+	@Override
+	public void playSound(World worldIn) {
+		worldIn.playSound((EntityPlayer)null, pos.getX(), pos.getY(), pos.getZ(), SoundHandler.CRUSHER, SoundCategory.BLOCKS, Utilities.getRandFloat(0.5F, 1.0F), Utilities.getRandFloat(0.2F, 1.0F));
 	}
 
 }
