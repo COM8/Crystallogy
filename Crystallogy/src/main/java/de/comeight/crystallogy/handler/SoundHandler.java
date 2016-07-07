720653de48c3ba492cf369dc8528c896377abe72
Fabian Sauter <sauter.fabian@online.de>
2016-07-07 13:44:02 +0200

    Added 3 new sound events

--- Crystallogy/src/main/java/de/comeight/crystallogy/handler/SoundHandler.java
+++ Crystallogy/src/main/java/de/comeight/crystallogy/handler/SoundHandler.java
@@ -8,6 +8,9 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class SoundHandler {
 	//-----------------------------------------------Variabeln:---------------------------------------------
 	public static SoundEvent CRUSHER;
+	public static SoundEvent COMPRESSOR;
+	public static SoundEvent LIGHT_WOOSH;
+	public static SoundEvent MACHINE_BROKEN;
 	public static SoundEvent EVIL_LAUGH_MALE_1;
 	public static SoundEvent EVIL_LAUGH_MALE_2;
 	
@@ -24,6 +27,21 @@ public class SoundHandler {
 		CRUSHER = new SoundEvent(rL);
 		GameRegistry.register(CRUSHER, rL);
 		
+		//COMPRESSOR:
+		rL = new ResourceLocation(CrystallogyBase.MODID, "compressor");
+		COMPRESSOR = new SoundEvent(rL);
+		GameRegistry.register(COMPRESSOR, rL);
+
+		//LIGHT_WOOSH:
+		rL = new ResourceLocation(CrystallogyBase.MODID, "light_woosh");
+		LIGHT_WOOSH = new SoundEvent(rL);
+		GameRegistry.register(LIGHT_WOOSH, rL);
+
+		//MACHINE_BROKEN:
+		rL = new ResourceLocation(CrystallogyBase.MODID, "machine_broken");
+		MACHINE_BROKEN = new SoundEvent(rL);
+		GameRegistry.register(MACHINE_BROKEN, rL);
+				
 		//EVIL_LAUGH_MALE_1:
 		rL = new ResourceLocation(CrystallogyBase.MODID, "evil_laugh_male_1");
 		EVIL_LAUGH_MALE_1 = new SoundEvent(rL);
