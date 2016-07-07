e97ef8488316b0413be80b21944b41bbbc9297b0
Fabian Sauter <sauter.fabian@online.de>
2016-07-07 14:18:19 +0200

    Fixed not moving

--- Crystallogy/src/main/java/de/comeight/crystallogy/items/EnderonCrystal.java
+++ Crystallogy/src/main/java/de/comeight/crystallogy/items/EnderonCrystal.java
@@ -58,7 +58,7 @@ public class EnderonCrystal extends BaseItem {
 	@SideOnly(Side.CLIENT)
 	private void spawnParticles(World worldIn, Vec3d playerPos){
 		ParticleEnderon eP = new ParticleEnderon(worldIn, getRandomParticleSpawn(playerPos), playerPos.addVector(0.0, 1.0, 0.0));
-		eP.setRBGColorF(1.0F, 1.0F, 0.0F);
+		eP.setRBGColorF(0.2F, 1.0F, 0.0F);
 		Minecraft.getMinecraft().effectRenderer.addEffect(eP);
 	}
 	
