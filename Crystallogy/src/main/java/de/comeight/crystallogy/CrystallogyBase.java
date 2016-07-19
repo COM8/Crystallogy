8fbdc51d679df79a6d398330e25a9678a465625b
Fabian Sauter <sauter.fabian@online.de>
2016-07-20 00:03:34 +0200

    Version 1.0.2 released

--- Crystallogy/src/main/java/de/comeight/crystallogy/CrystallogyBase.java
+++ Crystallogy/src/main/java/de/comeight/crystallogy/CrystallogyBase.java
@@ -14,7 +14,7 @@ public class CrystallogyBase {
 	//-----------------------------------------------Variabeln:---------------------------------------------
 	public static final String MODID = "crystallogy";
 	public static final String MODNAME = "Crystallogy";
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.0.2";
 	
 	@SidedProxy(clientSide="de.comeight.crystallogy.ClientProxy", serverSide= "de.comeight.crystallogy.ServerProxy")
 	public static CommonProxy proxy;
