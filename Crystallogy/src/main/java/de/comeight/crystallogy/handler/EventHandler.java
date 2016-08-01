8daa72c14edb8a15d2e4f3a84f2e1d2467879d72
COM8 <sauter.fabian@online.de>
2016-08-01 15:45:00 +0200

    Added a event to give the player the Book of Knowledge

--- Crystallogy/src/main/java/de/comeight/crystallogy/handler/EventHandler.java
+++ Crystallogy/src/main/java/de/comeight/crystallogy/handler/EventHandler.java
@@ -1,11 +1,16 @@
 package de.comeight.crystallogy.handler;
 
+import de.comeight.crystallogy.CrystallogyBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+import net.minecraftforge.items.ItemHandlerHelper;
 
 public class EventHandler {
 	//-----------------------------------------------Variabeln:---------------------------------------------
-
+	public static final String TAG_PLAYER_GOT_BOOK = CrystallogyBase.MODID + "gotSpawnBook";
 	
 	//-----------------------------------------------Constructor:-------------------------------------------
 
@@ -17,5 +22,21 @@ public class EventHandler {
 	@SubscribeEvent
 	public void onLootLoad(LootTableLoadEvent event) {
 		ChestGenHandler.init(event);
+	}
+	
+	@SubscribeEvent
+	public void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+		giveBookOfKnowledgeToPlayer(event);
+	}
+	
+	private void giveBookOfKnowledgeToPlayer(PlayerEvent.PlayerLoggedInEvent event){
+		if (ConfigHandler.shouldSpawnWithBook) {
+			NBTTagCompound compound = event.player.getEntityData();
+
+			if (!compound.getBoolean(TAG_PLAYER_GOT_BOOK)) {
+				ItemHandlerHelper.giveItemToPlayer(event.player, new ItemStack(ItemHandler.bookOfKnowledge));
+				compound.setBoolean(TAG_PLAYER_GOT_BOOK, true);
+			}
+		}
 	}
 }
