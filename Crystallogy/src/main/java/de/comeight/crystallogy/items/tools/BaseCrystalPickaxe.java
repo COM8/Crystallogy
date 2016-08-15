c2bc3382b4debe057dcc0d39f8733bd6d1cb0fb9
COM8 <sauter.fabian@online.de>
2016-08-15 18:11:29 +0200

    Added a mode switch to the pickaxe, closes #12

--- Crystallogy/src/main/java/de/comeight/crystallogy/items/tools/BaseCrystalPickaxe.java
+++ Crystallogy/src/main/java/de/comeight/crystallogy/items/tools/BaseCrystalPickaxe.java
@@ -3,15 +3,21 @@ package de.comeight.crystallogy.items.tools;
 import java.util.List;
 
 import de.comeight.crystallogy.util.ToolUtils;
+import de.comeight.crystallogy.util.Utilities;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
@@ -35,11 +41,20 @@ public class BaseCrystalPickaxe extends BaseItemPickaxe {
 		return rTR.sideHit.getOpposite();
 	}
 	
+	private int getMode(ItemStack itemStackIn){
+		if(!itemStackIn.hasTagCompound()){
+			itemStackIn.setTagCompound(new NBTTagCompound());
+		}
+		
+		NBTTagCompound compound = itemStackIn.getTagCompound();
+		return compound.getInteger("mode");
+	}
+	
 	//-----------------------------------------------Sonstige Methoden:-------------------------------------
 	@Override
 	public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos pos, EntityLivingBase entityLiving) {
-		if(!entityLiving.isSneaking() && state.getBlock().isToolEffective("pickaxe", state)){
-			breakBlocksArround(stack, worldIn, pos, entityLiving);
+		if(state.getBlock().isToolEffective("pickaxe", state)){
+			breakBlocksArround(stack, worldIn, pos, entityLiving, getMode(stack));
 		}
 		else{
 			if (state.getBlockHardness(worldIn, pos) > 0.0F)
@@ -50,41 +65,96 @@ public class BaseCrystalPickaxe extends BaseItemPickaxe {
 		return true;
 	}
 	
-	protected void breakBlocksArround(ItemStack stack, World worldIn, BlockPos pos, EntityLivingBase entityLiving){
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand) {
+		if(playerIn.isSneaking()){
+			nextMode(itemStackIn);
+			if(worldIn.isRemote){
+				playerIn.playSound(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0F, Utilities.getRandFloat(0.5F, 1.5F));
+				playerIn.addChatComponentMessage(new TextComponentString("New mode: " + TextFormatting.DARK_PURPLE + (getMode(itemStackIn) + 1)));
+			}
+		}
+		return super.onItemRightClick(itemStackIn, worldIn, playerIn, hand);
+	}
+	
+	protected void nextMode(ItemStack itemStackIn){
+		int mode = getMode(itemStackIn);
+		mode++;
+		if(mode > 2){
+			mode = 0;
+		}
+		NBTTagCompound compound = itemStackIn.getTagCompound();
+		compound.setInteger("mode", mode);
+	}
+	
+	protected void breakBlocksArround(ItemStack stack, World worldIn, BlockPos pos, EntityLivingBase entityLiving, int mode){
+		if(mode == 2){
+			return;
+		}
+		
 		EnumFacing direction = getDirectionPlayerIsFacing(worldIn, (EntityPlayer) entityLiving);
-		int aOE = getAOE();
-		int aOEHalf = (int) Math.round(((double) aOE)/2);
+		int aOEX = getAOE();
+		int aOEY = getAOE();
+		int aOEZ = getAOE();
+		int aOEHalf = (int) Math.round(((double) aOEX) / 2);
 		switch (direction) {
 			case DOWN:
 				pos = pos.add(0, -aOEHalf, 0);
+				if(mode == 1){
+					tryMineBlocks(pos, worldIn, stack, entityLiving, -aOEX, aOEX, 1, 2, -aOEZ, aOEZ);
+					return;
+				}
 				break;
 			case UP:
 				pos = pos.add(0, aOEHalf, 0);
+				if(mode == 1){
+					tryMineBlocks(pos, worldIn, stack, entityLiving, -aOEX, aOEX, -1, 0, -aOEZ, aOEZ);
+					return;
+				}
 				break;
 				
 			case WEST:
 				pos = pos.add(-aOEHalf, aOEHalf, 0);
+				if(mode == 1){
+					tryMineBlocks(pos, worldIn, stack, entityLiving, 1, 2, -aOEY, aOEY, -aOEZ, aOEZ);
+					return;
+				}
 				break;
 				
 			case EAST:
 				pos = pos.add(aOEHalf + 1, aOEHalf, 0);
+				if(mode == 1){
+					tryMineBlocks(pos, worldIn, stack, entityLiving, -2, -1, -aOEY, aOEY, -aOEZ, aOEZ);
+					return;
+				}
 				break;
 				
 			case NORTH:
 				pos = pos.add(0, aOEHalf, -aOEHalf);
+				if(mode == 1){
+					tryMineBlocks(pos, worldIn, stack, entityLiving, -aOEX, aOEX, -aOEY, aOEY, 1, 2);
+					return;
+				}
 				break;
 				
 			case SOUTH:
 				pos = pos.add(0, aOEHalf, aOEHalf + 1);
+				if(mode == 1){
+					tryMineBlocks(pos, worldIn, stack, entityLiving, -aOEX, aOEX, -aOEY, aOEY, -2, -1);
+					return;
+				}
 				break;
 
 			default:
 				break;
 		}
-		
-		for (int i = -aOE; i < aOE; i++) {
-			for (int j = -aOE; j < aOE; j++) {
-				for (int j2 = -aOE; j2 < aOE; j2++) {
+		tryMineBlocks(pos, worldIn, stack, entityLiving, -aOEX, aOEX, -aOEY, aOEY, -aOEZ, aOEZ);
+	}
+	
+	protected void tryMineBlocks(BlockPos pos, World worldIn, ItemStack stack, EntityLivingBase entityLiving, int xMin, int xMax, int yMin, int yMax, int zMin, int zMax){
+		for (int i = xMin; i < xMax; i++) {
+			for (int j = yMin; j < yMax; j++) {
+				for (int j2 = zMin; j2 < zMax; j2++) {
 					tryMineBlock(worldIn, pos.add(i, j, j2), stack, entityLiving);
 				}
 			}
@@ -115,10 +185,26 @@ public class BaseCrystalPickaxe extends BaseItemPickaxe {
 	@Override
 	public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced) {
 		int aoe = getAOE();
-		tooltip.add("Mines a " + TextFormatting.DARK_PURPLE + aoe * 2 + "x" + aoe * 2 + "x" + aoe * 2 + TextFormatting.RESET + TextFormatting.GRAY + " area.");
-		tooltip.add("");
-		tooltip.add("Hold " + TextFormatting.GOLD + TextFormatting.BOLD + "SHIFT" + TextFormatting.RESET + TextFormatting.GRAY + " to mine a " + TextFormatting.DARK_PURPLE + "1x1 " + TextFormatting.RESET + TextFormatting.GRAY + "area!");
+		int mode = getMode(stack);
 		
+		tooltip.add("Mode: " + TextFormatting.DARK_PURPLE + (mode + 1));
+		
+		switch (mode) {
+			case 0:
+				tooltip.add("Mines a " + TextFormatting.DARK_PURPLE + aoe * 2 + "x" + aoe * 2 + "x" + aoe * 2 + TextFormatting.RESET + TextFormatting.GRAY + " area.");
+				break;
+				
+			case 1:
+				tooltip.add("Mines a " + TextFormatting.DARK_PURPLE + aoe * 2 + "x" + aoe * 2 + "x1" + TextFormatting.RESET + TextFormatting.GRAY + " area.");
+				break;
+
+			default:
+				tooltip.add("Mines a " + TextFormatting.DARK_PURPLE + "1x1x1" + TextFormatting.RESET + TextFormatting.GRAY + " area.");
+				break;
+		}
+		
+		tooltip.add("");
+		tooltip.add("" + TextFormatting.GOLD + TextFormatting.BOLD + "SHIFT RIGHT-CLICK" + TextFormatting.RESET + TextFormatting.GRAY + " to change mode.");
 		super.addInformation(stack, playerIn, tooltip, advanced);
 	}
 	
--- Crystallogy/src/main/java/de/comeight/crystallogy/items/tools/BaseItemHammer.java
+++ Crystallogy/src/main/java/de/comeight/crystallogy/items/tools/BaseItemHammer.java
@@ -1,17 +1,12 @@
 package de.comeight.crystallogy.items.tools;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
@@ -33,7 +28,7 @@ public abstract class BaseItemHammer extends BaseItemPickaxe {
 	@Override
 	public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState blockIn, BlockPos pos, EntityLivingBase entityLiving) {
 		if(!entityLiving.isSneaking() && blockIn.getBlock().isToolEffective("pickaxe", blockIn)){
-			breakBlocksArround(stack, worldIn, blockIn, pos, entityLiving);
+			breakBlocksHammer(stack, worldIn, blockIn, pos, entityLiving);
 		}
 		else{
 			if ((double)blockIn.getBlockHardness(worldIn, pos) != 0.0D)
@@ -42,70 +37,6 @@ public abstract class BaseItemHammer extends BaseItemPickaxe {
 	        }
 		}
 		return true;
-	}
-	
-	protected void breakBlocksArround(ItemStack stack, World worldIn, IBlockState state, BlockPos pos, EntityLivingBase entityLiving){
-		Block block = state.getBlock();
-		if(state.getMaterial() == Material.AIR){
-			return;
-		}
-		RayTraceResult rTR = rayTrace(worldIn, (EntityPlayer) entityLiving, false);
-		ArrayList<BlockPos> list = calcAOE(stack, rTR, worldIn, pos);
-		for(BlockPos p : list){
-			mineBlock(worldIn, worldIn.getBlockState(p), p);
-		}
-		
-		if ((double)state.getBlockHardness(worldIn, pos) != 0.0D)
-        {
-            stack.damageItem(list.size() + 1, entityLiving);
-        }
-		
-	}
-	
-	protected void mineBlock(World worldIn, IBlockState state, BlockPos pos){
-		worldIn.destroyBlock(pos, true);
-		state.getBlock().onBlockDestroyedByPlayer(worldIn, pos, state);
-	}
-	
-	protected ArrayList<BlockPos> calcAOE(ItemStack stack, RayTraceResult rTR, World worldIn, BlockPos pos){
-		if(rTR == null){
-			return new ArrayList<BlockPos>();
-		}
-		
-		int refHarvestLevel = worldIn.getBlockState(pos).getBlock().getHarvestLevel(worldIn.getBlockState(pos));
-		ArrayList<BlockPos> list = new ArrayList<BlockPos>();
-		
-		if(rTR.sideHit == EnumFacing.EAST || rTR.sideHit == EnumFacing.WEST){
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX(), pos.getY(), pos.getZ() + 1), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX(), pos.getY(), pos.getZ() - 1), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX(), pos.getY() + 1, pos.getZ()), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX(), pos.getY() - 1, pos.getZ()), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX(), pos.getY() + 1, pos.getZ() + 1), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX(), pos.getY() + 1, pos.getZ() - 1), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX(), pos.getY() - 1, pos.getZ() + 1), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX(), pos.getY() - 1, pos.getZ() - 1), list);
-		}
-		else if(rTR.sideHit == EnumFacing.SOUTH || rTR.sideHit == EnumFacing.NORTH){
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX() + 1, pos.getY(), pos.getZ()), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX() - 1, pos.getY(), pos.getZ()), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX(), pos.getY() + 1, pos.getZ()), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX(), pos.getY() - 1, pos.getZ()), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX() + 1, pos.getY() + 1, pos.getZ()), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX() + 1, pos.getY() - 1, pos.getZ()), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX() - 1, pos.getY() + 1, pos.getZ()), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX() - 1, pos.getY() - 1, pos.getZ()), list);
-		}
-		else if(rTR.sideHit == EnumFacing.UP || rTR.sideHit == EnumFacing.DOWN){
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX() + 1, pos.getY(), pos.getZ()), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX() - 1, pos.getY(), pos.getZ()), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX(), pos.getY(), pos.getZ() + 1), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX(), pos.getY(), pos.getZ() - 1), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX() + 1, pos.getY(), pos.getZ() + 1), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX() + 1, pos.getY(), pos.getZ() - 1), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX() - 1, pos.getY(), pos.getZ() + 1), list);
-			canHarvestBlock(stack, worldIn, new BlockPos(pos.getX() - 1, pos.getY(), pos.getZ() - 1), list);
-		}
-		return list;
 	}
 	
 	@Override
