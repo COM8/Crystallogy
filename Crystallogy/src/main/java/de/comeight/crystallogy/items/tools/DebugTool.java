83373868cf8347f767e102a2e5a8f1b52c63616d
COM8 <sauter.fabian@online.de>
2016-09-15 14:41:06 +0200

    Enhanced debug tool functionality

--- Crystallogy/src/main/java/de/comeight/crystallogy/items/tools/DebugTool.java
+++ Crystallogy/src/main/java/de/comeight/crystallogy/items/tools/DebugTool.java
@@ -1,17 +1,25 @@
 package de.comeight.crystallogy.items.tools;
 
+import java.util.List;
+
 import de.comeight.crystallogy.items.BaseItem;
 import de.comeight.crystallogy.particles.ParticleDebug;
+import de.comeight.crystallogy.tileEntitys.BaseTileEntity;
+import de.comeight.crystallogy.util.EnumDebugToolModes;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -33,6 +41,9 @@ public class DebugTool extends BaseItem {
 	//-----------------------------------------------Sonstige Methoden:-------------------------------------
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand) {
+		if(playerIn.isSneaking()){
+			nextMode(itemStackIn, playerIn);
+		}
 		return super.onItemRightClick(itemStackIn, worldIn, playerIn, hand);
 	}
 	
@@ -43,11 +54,43 @@ public class DebugTool extends BaseItem {
 	
 	@Override
 	public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-		if(worldIn.isRemote){
-			spawnDebugParticle(worldIn, new Vec3d(pos.getX() + hitX, pos.getY() + hitY, pos.getZ() + hitZ));
+		EnumDebugToolModes mode = getMode(stack);
+		switch (mode) {
+			case BLOCKUPDATE:
+				worldIn.getBlockState(pos).getBlock().updateTick(worldIn, pos, worldIn.getBlockState(pos), worldIn.rand);
+				break;
+				
+			case REQSYNC:
+				if(worldIn.isRemote){
+					TileEntity tE = worldIn.getTileEntity(pos);
+					if(tE != null && tE instanceof BaseTileEntity){
+						BaseTileEntity btE = (BaseTileEntity) tE;
+						btE.requestSync();
+					}
+				}
+				break;
+				
+			case SYNC:
+				if(!worldIn.isRemote){
+					TileEntity tE = worldIn.getTileEntity(pos);
+					if(tE != null && tE instanceof BaseTileEntity){
+						BaseTileEntity btE = (BaseTileEntity) tE;
+						btE.sync();
+					}
+				}
+				break;
+				
+			case SPAWNPARTICLES:
+				if(worldIn.isRemote){
+					spawnDebugParticle(worldIn, new Vec3d(pos.getX() + hitX, pos.getY() + hitY, pos.getZ() + hitZ));
+				}
+				break;
+
+			default:
+				return EnumActionResult.PASS;
 		}
 		
-		return super.onItemUse(stack, playerIn, worldIn, pos, hand, facing, hitX, hitY, hitZ);
+		return EnumActionResult.SUCCESS;
 	}
 	
 	@SideOnly(Side.CLIENT)
@@ -55,6 +98,43 @@ public class DebugTool extends BaseItem {
 		ParticleDebug pD = new ParticleDebug(worldIn, pos);
 		pD.setCanTickToDeath(false);
 		Minecraft.getMinecraft().effectRenderer.addEffect(pD);
+	}
+	
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced) {
+		tooltip.add("Mode: " + TextFormatting.DARK_PURPLE + getMode(stack).getName());
+		super.addInformation(stack, playerIn, tooltip, advanced);
+	}
+	
+	private void nextMode(ItemStack itemStackIn, EntityPlayer playerIn) {
+		int curMode = getMode(itemStackIn).getId();
+		NBTTagCompound compound = itemStackIn.getTagCompound();
+		EnumDebugToolModes mode = EnumDebugToolModes.fromID(EnumDebugToolModes.getNextMode(curMode));
+		compound.setInteger("mode", mode.getId());
+		itemStackIn.setTagCompound(compound);
+		
+		if(playerIn.worldObj.isRemote){
+			playerIn.addChatMessage(new TextComponentString("Mode: " + TextFormatting.DARK_PURPLE + mode.getName()));
+		}
+	}
+	
+	private EnumDebugToolModes getMode(ItemStack itemStackIn) {
+		if(!itemStackIn.hasTagCompound()){
+			NBTTagCompound compound = new NBTTagCompound();
+			compound.setInteger("mode", 0);
+			itemStackIn.setTagCompound(compound);
+			return EnumDebugToolModes.fromID(0);
+		}
+		
+		NBTTagCompound compound = itemStackIn.getTagCompound();
+		if(!compound.hasKey("mode")){
+			compound.setInteger("mode", 0);
+			itemStackIn.setTagCompound(compound);
+			return EnumDebugToolModes.fromID(0);
+		}
+		else{
+			return EnumDebugToolModes.fromID(compound.getInteger("mode"));
+		}
 	}
 	
 }
