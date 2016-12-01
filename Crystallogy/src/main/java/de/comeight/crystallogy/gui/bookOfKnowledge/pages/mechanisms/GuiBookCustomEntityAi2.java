ece569a13a6a35cfd1cd595dddf748d04cbc9479
COM8 <sauter.fabian@online.de>
2016-12-01 14:56:15 +0100

    Typo

--- Crystallogy/src/main/java/de/comeight/crystallogy/gui/bookOfKnowledge/pages/mechanisms/GuiBookCustomEntityAi2.java
+++ Crystallogy/src/main/java/de/comeight/crystallogy/gui/bookOfKnowledge/pages/mechanisms/GuiBookCustomEntityAi2.java
@@ -57,7 +57,7 @@ public class GuiBookCustomEntityAi2 extends GuiBookPageSuggestions {
 				+ "\n"
 				+ "If you want to specifie, what should happen if the entity finishes it's job "
 				+ "or wether it should get teleported to it's target if it gets stuck in some place,"
-				+ "you can use the Dessecting Table for this.");
+				+ "you can use the Dissecting Table for this.");
 	}
 	
 	private void initRecipe(){
